fb26c13be7ae7171a66425abdbbc4cdd0157e8d8
kouki-16 <kouki-16@og.com>
2025-08-21 13:51:16 +0900

    tip calculator

--- practice-project/src/TipCalculator.js
+++ practice-project/src/TipCalculator.js
@@ -3,11 +3,25 @@ import { useState } from "react";
 export default function App(){
     const [bill, setBill] = useState("")
     const [totalBill, setTotalBill] = useState(0);
+    const [myPersentage, setMyPercentage] = useState(0);
+    const [friendPercentage, setFriendPercentage] = useState(0);
+    const tips = bill * ((myPersentage + friendPercentage) / 2 / 100);
+    const totalPayble = bill + tips;
+
+    function handleReset(){
+        setBill(0);
+        setMyPercentage(0);
+        setFriendPercentage(0);
+    }
+
     
     return (
         <div>
             <BillInput bill={bill} onSetBill = {setBill} setTotalBill ={setTotalBill}/>
-            <TotalBill totalBill={totalBill} />
+            <ServiceRelated myPersentage={myPersentage} setMyPercentage={setMyPercentage}>How did you like the service?</ServiceRelated>
+            <ServiceRelated myPersentage={friendPercentage} setMyPercentage={setFriendPercentage}>How did you friend like the service?</ServiceRelated>
+            <TotalBill totalBill={totalPayble} tips={tips} bill={bill} />
+            <ResetForm handleReset={handleReset} />
         </div>
     );
 }
@@ -31,10 +45,36 @@ function BillInput({bill, onSetBill, setTotalBill}){
     );
 }
 
-function TotalBill({totalBill}){
+function ServiceRelated({myPersentage, setMyPercentage, children}){
+    return (
+        <div>
+            <label>{children}</label>
+            <select value={myPersentage} onChange={(e)=>setMyPercentage(Number(e.target.value))}>
+                <option value="0">Desatisfied (0%)</option>
+                <option value="5">It was ok (5%)</option>
+                <option value="10">It was good (10%)</option>
+                <option value="20">Absoulatly Amazing (20%)</option>
+            </select>
+        </div>
+    );
+}
+
+function TotalBill({totalBill, tips, bill}){
+
     return(
         <div>
-            <h4>You total Bill is {totalBill}</h4>
+            <h4>`Your total payble bill is {totalBill} 
+                 -  Actual amount is {bill}
+                Tips {tips}`
+                </h4>
+        </div>
+    );
+}
+
+function ResetForm({handleReset}){
+    return(
+        <div>
+            <button onClick={handleReset}>Reset</button>
         </div>
     );
 }
