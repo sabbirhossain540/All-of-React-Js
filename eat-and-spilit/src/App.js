9c61c8a9c8a839726842631555487aab497c9f5b
kouki-16 <kouki-16@og.com>
2025-08-21 15:54:52 +0900

    fdfbd

---
+++ eat-and-spilit/src/App.js
@@ -0,0 +1,125 @@
+import { useState } from "react";
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+function Button({children, onClick}){
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+
+export default function App(){
+  const [showAddFriend, setShowAddFriend] = useState(false)
+
+  function handleShowAddFriend(){
+    setShowAddFriend((show)=>!show);
+  }
+
+  return(
+    <div className="app">
+      <div className="sidebar">
+        <FriendList/>
+        {showAddFriend && <FormAddFried/>}
+        <Button onClick={handleShowAddFriend}>{showAddFriend ? "Close" : "Add Friend"}</Button>
+      </div>
+
+      <FormSplitBill/>
+    </div>
+  );
+}
+
+function FriendList(){
+  const friends = initialFriends;
+  return <ul>
+    {friends.map((friend)=>(
+      <Freind friend={friend} key={friend.key} />
+    ))}
+  </ul>;
+}
+
+function Freind({friend, key}){
+  return <li>
+    <img src={friend.image} alt={friend.name} />
+    <h3>{friend.name}</h3>
+    {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {friend.balance}  yen
+        </p>
+      )}
+
+    {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owe you {friend.balance} yen
+        </p>
+      )}
+
+    {friend.balance === 0 && (
+        <p >
+          You and {friend.name} even
+        </p>
+      )}
+
+    <Button>Select</Button>
+  </li>;
+}
+
+
+
+
+
+
+function FormAddFried(){
+  return <form className="form-add-friend">
+    <label>👫Friend Name</label>
+    <input type="text"/>
+
+    <label>🌄Image Url</label>
+    <input type="text"/>
+
+    <Button>Add</Button>
+  </form>
+}
+
+function FormSplitBill(){
+  return <form className="form-split-bill">
+    <h2>Spilit a bill with X</h2>
+    <label>💰Bill Value</label>
+    <input type="text"/>
+
+    <label>🧍‍♀️Your Expense</label>
+    <input type="text"/>
+
+    <label>👫Friend Expense</label>
+    <input type="text" disabled/>
+
+     <label>🤑 Who is paying the bill</label>
+     <select>
+      <option value="user">You</option>
+      <option value="Friends">X</option>
+     </select>
+
+    <Button>Spilit Bill</Button>
+  </form>
+}
