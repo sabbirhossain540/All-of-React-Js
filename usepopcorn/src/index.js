36ad5afb79d24f5342aac3463be2eff57f1fe84a
kouki-16 <kouki-16@og.com>
2025-08-29 16:22:02 +0900

    d

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { use, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 import StarRating from './StarRating';
 
+function Test(){
+  const [movieRating, setMovieRating] = useState(0);
+  return(
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={ setMovieRating }/>
+      <p>This is was rated {movieRating} starts</p>
+    </div>
+
+  );
+
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating maxRating={5} messages = {["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
+    <StarRating 
+      maxRating={5} 
+      color="red"
+      size={28}
+      defaultRaiting={3}
+    />
+    <Test/>
   </React.StrictMode>
 );
 
