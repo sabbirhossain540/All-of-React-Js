9ef5d731cb621fd8f9c13c92c11af5d7859dd34f
kouki-16 <kouki-16@og.com>
2025-09-01 16:57:18 +0900

    dfsdg

--- usepopcorns/map/src/index.js
+++ usepopcorns/map/src/index.js
@@ -1,7 +1,7 @@
 import React, { use, useState } from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
+import './index.css';
+import App from './App';
 import StarRating from './StarRating';
 
 function Test(){
@@ -19,15 +19,17 @@ function Test(){
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} messages = {["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
+    <App />
+    {/* <StarRating maxRating={5} messages = {["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
     <StarRating 
       maxRating={5} 
       color="red"
       size={28}
       defaultRaiting={3}
     />
-    <Test/>
+    <Test/> */}
+
+
   </React.StrictMode>
 );
 
